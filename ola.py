d9a6f363985347f93d4fd2fe7aca81a799b9b470
oladimeji ajayi <olaajayi2424@gmail.com>
2024-01-10 09:31:39 -0800

    my first commit

---
+++ ola.py
@@ -0,0 +1,8 @@
+def main ():
+  name = input("what is your name?")
+  print("we want to know if you like programming!")
+  answer = input("do you like programming"+ name +"?")
+  print("great! You said" + answer + "?")
+  print("lets learn some python today")
+if __name__== "__main__":
+    main()
---
+++ oladimeji.py
@@ -0,0 +1,10 @@
+def main ():
+  name = "oladimeji ajayi"
+  print("We want to know if u like programming!")
+  print()
+  print("do you like programming "+ name +"?")
+  answer = input()
+  print("Great! You said " + answer + "?")
+  Print ("let's start learning some python today")
+if __name__=="__main__":
+    main()
